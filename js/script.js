8477a4675c8643e06bb5414fd472c4a1e6c93a47
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-04-23 11:10:54 +0200

    Mobile Menu & JavaScript

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+// Hamburger menu functionallity (for smaller screens)
+const btn = document.getElementById('menu-btn') // To target the button for the burger menu
+const menu = document.getElementById('menu')    // To target the burger menu itself
 
-btn.addEventListener('click', navToggle)
+btn.addEventListener('click', navToggle)        // To add an event listener to the button
 
-function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
-  menu.classList.toggle('hidden')
+function navToggle() {                          // The function which will run after clicking the button
+  btn.classList.toggle('open')                  // The method toggle() add / removes the class of open (transforms the three lines of the burger menu into an X)
+  menu.classList.toggle('flex')                 // The method toggle() add / removes the class of flex  (shows the menu)
+  menu.classList.toggle('hidden')               // The method toggle() add / removes the class of hidden  (hides the menu)
 }
